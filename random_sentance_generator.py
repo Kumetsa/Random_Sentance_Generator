4b4b60afcca339e9dfa6f22e68ca22158fd7cc4a
Kumetsa <i.e.ivanov1989@gmail.com>
2023-02-20 16:40:34 +0200

    Initial logic
    Initial logic for random sentance generator along with file for soring variables.

--- random_sentance_generator.py
+++ random_sentance_generator.py
@@ -1,1 +1,20 @@
+import variables
+import random
 
+
+def get_random_word(words):
+    return random.choice(words)
+
+
+print("Hello, this is Random Sentance Generator !")
+input(f'Click [Enter] to generate your first sentence.')
+
+while True:
+    random_name = get_random_word(variables.names)
+    random_place = get_random_word(variables.places)
+    random_verb = get_random_word(variables.verbs)
+    random_noun = get_random_word(variables.nouns)
+    random_adverb = get_random_word(variables.adverbs)
+    random_detail = get_random_word(variables.details)
+    print(f'{random_name} from {random_place} {random_adverb} {random_verb} {random_noun} {random_detail}.')
+    input(f'Click [Enter] to generate a new one!')
---
+++ variables.py
@@ -0,0 +1,31 @@
+names = ['Adam', 'Amelia', 'Benjamin', 'Charlotte', 'Christopher', 'Claire', 'David', 'Elizabeth', 'Emily', 'Ethan',
+         'Grace', 'Henry', 'Isabella', 'Jack', 'Jacob', 'James', 'Lily', 'Lucy', 'Madison', 'Matthew', 'Mia', 'Michael',
+         'Nathan', 'Olivia', 'Patrick', 'Rachel', 'Samuel', 'Sarah', 'Sophia', 'Thomas', 'Victoria', 'William',
+         'Abigail', 'Alexander', 'Alyssa', 'Andrew', 'Ava', 'Caroline', 'Charles', 'Chloe']
+
+places = ['Amsterdam', 'Bangkok', 'Barcelona', 'Beijing', 'Berlin', 'Buenos Aires', 'Cairo', 'Cape Town', 'Dubai',
+          'Dublin', 'Florence', 'Hanoi', 'Istanbul', 'Jerusalem', 'Kuala Lumpur', 'Lisbon', 'London', 'Los Angeles',
+          'Madrid', 'Marrakech', 'Melbourne', 'Moscow', 'Mumbai', 'New York', 'Paris', 'Rio de Janeiro', 'Rome',
+          'San Francisco', 'Seoul', 'Shanghai', 'Singapore', 'Sydney', 'Taipei', 'Tokyo', 'Toronto', 'Vancouver',
+          'Venice', 'Vienna', 'Warsaw', 'Zurich']
+
+verbs = ['run', 'jump', 'eat', 'sleep', 'study', 'read', 'write', 'speak', 'listen', 'think', 'watch', 'dance', 'swim',
+         'play', 'work', 'cook', 'travel', 'love', 'hate', 'learn']
+
+nouns = ['car', 'house', 'book', 'tree', 'computer', 'phone', 'table', 'chair', 'door', 'window', 'shirt', 'shoe',
+         'hat', 'bag', 'pen', 'pencil', 'paper', 'music', 'movie', 'television', 'food', 'water', 'coffee', 'tea',
+         'money', 'time', 'friend', 'family', 'job', 'school', 'city', 'country', 'sky', 'sun', 'moon', 'ocean',
+         'river', 'beach', 'park', 'museum']
+
+adverbs = ['quickly', 'slowly', 'easily', 'hard', 'fast', 'well', 'badly', 'loudly', 'quietly', 'always', 'never',
+           'often', 'sometimes', 'rarely', 'already', 'soon', 'yesterday', 'today', 'tomorrow', 'here', 'there',
+           'everywhere', 'nowhere', 'abroad', 'home', 'together', 'apart', 'enough', 'not', 'also', 'even', 'just',
+           'really', 'only', 'almost', 'always', 'simply', 'basically', 'finally', 'possibly']
+
+details = ['on the top floor', 'by the river', 'in the forest', 'at the beach', 'on the mountain', 'under the bridge',
+           'in the garden', 'at the mall', 'in the city center', 'at the airport', 'at the train station',
+           'in the classroom', 'at the office', 'on the balcony', 'in the library', 'at the restaurant',
+           'in the swimming pool', 'on the street corner', 'in the parking lot', 'at the grocery store']
+
+
+
